27d908a374196ad03378593b1350a2d77230f51b
nitaiaharoni1 <nitaiaharoni1@gmail.com>
2018-02-27 19:05:34 +0200

    Heroku2

--- server.js
+++ server.js
@@ -17,30 +17,42 @@ const options = {
     //format: 'simple'
 };
 
-const recognizer = new speechService(options);
-recognizer.start((error, service) => {
-    if (!error) {
-        console.log('recognition started');
-        service.sendFile('./audio1_short.wav');
-        service.on('recognition', (text) => {
-            if (text.RecognitionStatus === 'Success') {
-                console.log(text);
-                keyPhrases(text.DisplayText).then(res => JSON.parse(res)).then(res => {
-                    console.log(res.documents)
-                    //console.log(res.errors)
-                    //res.documents[0].keyPhrases[1]
-                    wiki().search(res.documents[0].keyPhrases[4], 10).then(data =>{
-                        console.log(data.results[0]);
-                        wiki().page(data.results[0]).then(page => {
-                            page.summary().then(console.log);
-                            page.mainImage().then(console.log);
+var app = express();
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+var port = process.env.PORT || 5000;
+
+app.post('/audio', function(req, res) {
+    var audio = req.body;
+
+    const recognizer = new speechService(options);
+    recognizer.start((error, service) => {
+        if (!error) {
+            console.log('recognition started');
+            service.sendFile('./audio1_short.wav');
+            service.on('recognition', (text) => {
+                if (text.RecognitionStatus === 'Success') {
+                    console.log(text);
+                    keyPhrases(text.DisplayText).then(res => JSON.parse(res)).then(res => {
+                        console.log(res.documents)
+                        //console.log(res.errors)
+                        //res.documents[0].keyPhrases[1]
+                        wiki().search(res.documents[0].keyPhrases[4], 10).then(data => {
+                            console.log(data.results[0]);
+                            wiki().page(data.results[0]).then(page => {
+                                page.summary().then(console.log);
+                                page.mainImage().then(console.log);
+                            });
                         });
-                    });
-                }).catch(err => console.error(err))
-            }
-        });
-    }
+                    }).catch(err => console.error(err))
+                }
+            });
+        }
+        res.send('success');
+    });
 });
+
+app.listen(port);
 
 
 
