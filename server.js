5f731d0bbfb6141c964263e66539096cd4653dc2
nitaiaharoni1 <nitaiaharoni1@gmail.com>
2018-03-14 20:35:46 +0200

    end of the day

--- server.js
+++ server.js
@@ -6,7 +6,10 @@ const cogserv = require('cogserv-text-analytics');
 process.env.COGSERV_TEXT_KEY = '7ed66a7ac625436f8b202ff503d5f336';
 const {keyPhrases, sentiment} = require('./text-analytics')
 
+//Wikipedia API
 const wiki = require('wikijs').default;
+
+//File Upload API
 const fileUpload = require('express-fileupload');
 
 //Bing Speech API
@@ -29,16 +32,10 @@ app.get("/", function (req, res) {
 });
 
 app.post('/upload', function (req, res) {
-    console.log(req.files.file);
-    let sampleFile = req.files.file;
-    sampleFile.mv('./' + sampleFile.name, function (err) {
-        if (err)
-            return res.status(500).send(err);
-    });
-
+    uploaded(req.files.file);
     const recognizer = new speechService(options);
     recognizer.start((error, service) => {
-        let Out = {};
+        Out = {};
         if (!error) {
             console.log('Recognition Started!');
             service.sendFile('./' + sampleFile.name);
@@ -47,10 +44,11 @@ app.post('/upload', function (req, res) {
                     Out.text = speechtotext.DisplayText;
                     Out.terms = [];
                     console.log("Out Object text:", Out);
-                    res.send(Out);
+                    //res.send(Out);
                     keyPhrases(speechtotext.DisplayText).then(resPhrases => {
                         resPhrases = JSON.parse(resPhrases);
                         console.log("ResPhrases Here:", resPhrases.documents[0].keyPhrases);
+                        //wikiTerms(resPhrases.documents[0].keyPhrases).then(res.send(Out));
                         var counter = 0;
                         resPhrases.documents[0].keyPhrases.forEach(function (item, i, array) {
                             Out.terms.push({title: item});
@@ -80,7 +78,6 @@ app.post('/upload', function (req, res) {
                         });
                     }).catch(err => console.error(err))
                 }
-
             });
         }
     });
@@ -88,58 +85,43 @@ app.post('/upload', function (req, res) {
 
 app.listen(port);
 
+//Functions
+function uploaded(file) {
+    sampleFile = file;
+    sampleFile.mv('./' + sampleFile.name, function (err) {
+        if (err)
+            return res.status(500).send(err);
+    });
+}
 
-// var app = express();
-// app.use(bodyParser.json()); // support json encoded bodies
-// app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
-// var port = process.env.PORT || 5000;
-// app.listen(port);
-//
-//
-//
-//
-// app.post('/audio', function(req, res) {
-//     var audio = req.body;
-//
-//     const recognizer = new speechService(options);
-//     recognizer.start((error, service) => {
-//         if (!error) {
-//             console.log('recognition started');
-//             service.sendFile(audio);
-//             service.on('recognition', (text) => {
-//                 if (text.RecognitionStatus === 'Success') {
-//                     console.log(text);
-//                     keyPhrases(text.DisplayText).then(res => JSON.parse(res)).then(res => {
-//                         console.log(res.documents)
-//                         //console.log(res.errors)
-//                         wiki().search(res.documents[0].keyPhrases[0], 10).then(data =>{
-//                             console.log(data.results[0]);
-//                             wiki().page(data.results[0]).then(page => {
-//                                 page.summary().then(console.log);
-//                                 page.mainImage().then(console.log);
-//                             });
-//                         });
-//                     }).catch(err => console.error(err))
-//                 }
-//             });
-//         }
-//     });
-//
-//     res.send(user_id + ' ' + token + ' ' + geo);
-// });
+function wikiTerms(phrases) {
+        var counter = 0;
+        phrases.forEach(function (item, i, array) {
+            Out.terms.push({title: item});
+            //console.log("Out Object with titles:", Out);
 
-
-// var GoogleSearch = require('google-search');
-// var googleSearch = new GoogleSearch({
-//     key: 'AIzaSyA7BSZjJBbYXyqqUJV-REm4PSNmrMwVxJA',
-//     cx: '007115896535282779850:zjkhmuunzj4'
-// });
-//
-// googleSearch.build({
-//     q: "Bitcoin",
-//     start: 1,
-//     num: 2, // Number of search results to return between 1 and 10, inclusive
-//     googlehost: "http://www.google.com" // Restricts results to URLs from a specified site
-// }, function(error, response) {
-//     console.log(response);
-// });
+            wiki().search(item, 10).then(data => {
+                //which result to choose??? 0, 1 or 2?? console.log(i + " wiki search:", data.results[0]);
+                wiki().page(data.results[0]).then(page => {
+                    page.summary().then(summary => {
+                        Out.terms[i].summary = summary;
+                        counter = counter + 0.5;
+                        if (counter == array.length) {
+                            console.log("Out: ", Out);
+                            return;
+                            //res.send(Out);
+                        }
+                    });
+                    page.mainImage().then(mainImage => {
+                        Out.terms[i].image = mainImage;
+                        counter = counter + 0.5;
+                        if (counter == array.length) {
+                            console.log("Out: ", Out);
+                            return;
+                            //res.send(Out);
+                        }
+                    });
+                });
+            });
+        });
+}
