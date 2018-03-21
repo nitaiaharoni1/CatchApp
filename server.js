303a5d72c3394319ef1c171402395ec722a9d4de
nitaiaharoni1 <nitaiaharoni1@gmail.com>
2018-03-21 22:08:49 +0200

    yesh

--- server.js
+++ server.js
@@ -1,17 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
 //Text Analytics
 const cogserv = require('cogserv-text-analytics');
 process.env.COGSERV_TEXT_KEY = '7ed66a7ac625436f8b202ff503d5f336';
 const {keyPhrases, sentiment} = require('./text-analytics')
-
 //Wikipedia API
 const wiki = require('wikijs').default;
-
 //File Upload API
 const fileUpload = require('express-fileupload');
-
 //Bing Speech API
 const speechService = require('ms-bing-speech-service');
 const options = {
@@ -25,103 +21,148 @@ var app = express();
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({extended: true})); // support encoded bodies
 app.use(fileUpload());
-var port = process.env.PORT || 8015;
+var port = process.env.PORT || 8010;
 
 app.get("/", function (req, res) {
     res.send("Welcome")
 });
 
 app.post('/upload', function (req, res) {
-    uploaded(req.files.file);
-    const recognizer = new speechService(options);
-    recognizer.start((error, service) => {
-        Out = {};
-        if (!error) {
-            console.log('Recognition Started!');
-            service.sendFile('./' + sampleFile.name);
-            service.on('recognition', (speechtotext) => {
-                if (speechtotext.RecognitionStatus === 'Success') {
-                    Out.text = speechtotext.DisplayText;
-                    Out.terms = [];
-                    console.log("Out Object text:", Out);
-                    //res.send(Out);
-                    keyPhrases(speechtotext.DisplayText).then(resPhrases => {
-                        resPhrases = JSON.parse(resPhrases);
-                        console.log("ResPhrases Here:", resPhrases.documents[0].keyPhrases);
-                        //wikiTerms(resPhrases.documents[0].keyPhrases).then(res.send(Out));
-                        var counter = 0;
-                        resPhrases.documents[0].keyPhrases.forEach(function (item, i, array) {
-                            Out.terms.push({title: item});
-                            //console.log("Out Object with titles:", Out);
-
-                            wiki().search(item, 10).then(data => {
-                                //which result to choose??? 0, 1 or 2?? console.log(i + " wiki search:", data.results[0]);
-                                wiki().page(data.results[0]).then(page => {
-                                    page.summary().then(summary => {
-                                        Out.terms[i].summary = summary;
-                                        counter = counter + 0.5;
-                                        if(counter == array.length){
-                                            console.log("Out: ", Out);
-                                            res.send(Out);
-                                        }
-                                    });
-                                    page.mainImage().then(mainImage => {
-                                        Out.terms[i].image = mainImage;
-                                        counter = counter + 0.5;
-                                        if(counter == array.length){
-                                            console.log("Out: ", Out);
-                                            res.send(Out);
-                                        }
-                                    });
-                                });
-                            });
-                        });
-                    }).catch(err => console.error(err))
-                }
+    let filePath = './' + req.files.file.name;
+    let analyzedText;
+    let out = {};
+    let phrases;
+    let userLang = req.body.userLanguage;
+    SpeechToText(filePath).then(retAnalyzedText => {
+        analyzedText = retAnalyzedText;
+        console.log(analyzedText);
+        getPhrases(analyzedText).then(retPhrases => {
+            phrases = retPhrases;
+            console.log(phrases);
+            wikiLoop(phrases, userLang).then(retWikiTerms => {
+                wikiTerms = retWikiTerms;
+                console.log(phrases);
+                out.text = analyzedText;
+                out.terms = phrases;
+                out.wiki = wikiTerms;
+                res.send(out);
             });
-        }
+        });
     });
 });
 
 app.listen(port);
 
 //Functions
-function uploaded(file) {
-    sampleFile = file;
-    sampleFile.mv('./' + sampleFile.name, function (err) {
-        if (err)
-            return res.status(500).send(err);
+async function SpeechToText(filePath) {
+    var retText = await new Promise(resolve => {
+        let recognizer = new speechService(options);
+        let analyzedText;
+        recognizer.start((error, service) => {
+            if (!error) {
+                console.log('Recognition Started!');
+                service.sendFile(filePath);
+                service.on('recognition', (speechtotext) => {
+                    if (speechtotext.RecognitionStatus === 'Success') {
+                        analyzedText = speechtotext.DisplayText;
+                        resolve(analyzedText);
+                    }
+                });
+            }
+        });
     });
+    return retText;
 }
 
-function wikiTerms(phrases) {
-        var counter = 0;
-        phrases.forEach(function (item, i, array) {
-            Out.terms.push({title: item});
-            //console.log("Out Object with titles:", Out);
+async function getPhrases(text) {
+    var retPhrases = await new Promise(resolve => {
+        let phrases;
+        keyPhrases(text).then(resPhrases => {
+            resPhrases = JSON.parse(resPhrases);
+            phrases = resPhrases.documents[0].keyPhrases;
+            resolve(phrases);
+        }).catch(err => console.error(err));
+    });
+    return retPhrases;
+}
 
-            wiki().search(item, 10).then(data => {
-                //which result to choose??? 0, 1 or 2?? console.log(i + " wiki search:", data.results[0]);
-                wiki().page(data.results[0]).then(page => {
-                    page.summary().then(summary => {
-                        Out.terms[i].summary = summary;
-                        counter = counter + 0.5;
-                        if (counter == array.length) {
-                            console.log("Out: ", Out);
-                            return;
-                            //res.send(Out);
+async function wikiTerm(term, userLang) {
+    var retWikiTerm = await new Promise(resolve => {
+        let langCountry;
+        let langSearch;
+        let obj = {};
+        let counter = 0;
+        wiki().search(term, 1).then(searched => {
+            //which result to choose??? 0, 1 or 2?
+            wiki().page(searched.results[0]).then(page => {
+                try {
+                    page.langlinks().then(langsArray => {
+                        if(langsArray.length<5){    //does not return object with less then 5 translations
+                            resolve({});
                         }
-                    });
-                    page.mainImage().then(mainImage => {
-                        Out.terms[i].image = mainImage;
-                        counter = counter + 0.5;
-                        if (counter == array.length) {
-                            console.log("Out: ", Out);
-                            return;
-                            //res.send(Out);
+                        for (var i = 0; i < langsArray.length; i++) {
+                            if (langsArray[i].lang == userLang) {
+                                langSearch = langsArray[i].title;
+                                langCountry = userLang;
+                                break;
+                            } else {
+                                langSearch = searched.results[0];
+                                langCountry = "en";
+                            }
                         }
+                        wiki({apiUrl: 'http://' + langCountry + '.wikipedia.org/w/api.php'}).page(langSearch).then(page => {
+                            obj.title = langSearch;
+                            obj.url = page.raw.fullurl;
+                            page.summary().then(summary => {
+                                obj.summery = summary;
+                                counter = counter + 0.5;
+                                if (counter == 1) {
+                                    resolve(obj);
+                                }
+                            }).catch(err => {
+                                obj.summery = "";
+                                counter = counter + 0.5;
+                                if (counter == 1) {
+                                    resolve(obj);
+                                }
+                            });
+                            page.mainImage().then(mainImage => {
+                                obj.image = mainImage;
+                                counter = counter + 0.5;
+                                if (counter == 1) {
+                                    resolve(obj);
+                                }
+                            }).catch(err => {
+                                obj.image = "";
+                                counter = counter + 0.5;
+                                if (counter == 1) {
+                                    resolve(obj);
+                                }
+                            });
+                        });
                     });
-                });
+                } catch (error) {
+                    console.error(error);
+                }
             });
         });
+    });
+    return retWikiTerm;
+}
+
+async function wikiLoop(phrases, userLang) {
+    var retWikiTerms = await new Promise(resolve => {
+        var counter = 0;
+        let array = [];
+        phrases.forEach(function (term, i) {
+            wikiTerm(term, userLang).then(obj => {
+                array.push(obj);
+                counter++;
+                if (counter == phrases.length) {
+                    resolve(array);
+                }
+            });
+        });
+    });
+    return retWikiTerms;
 }
