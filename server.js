c5d2e112354388332615959abe4ede548282e5e4
nitaiaharoni1 <nitaiaharoni1@gmail.com>
2018-05-01 01:08:15 +0300

    asdasf

--- server.js
+++ server.js
@@ -1,16 +1,25 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-//Text Analytics
-const cogserv = require('cogserv-text-analytics');
+
+const fs = require('fs');
+
+const speech = require("@google-cloud/speech"); // Google Speech API
+const configGoogle = {
+    projectId: 'dulcet-pilot-202710',
+    keyFilename: './google-speech/Catchapp-24f90e9bc6d3.json'
+};
+
+
+const cogserv = require('cogserv-text-analytics'); //Text Analytics
 process.env.COGSERV_TEXT_KEY = '996f7b4c6b924383b3fe595509a7fec1';
 const {keyPhrases, sentiment} = require('./text-analytics')
-//Wikipedia API
-const wiki = require('wikijs').default;
-//File Upload API
-const fileUpload = require('express-fileupload');
-//Bing Speech API
-const speechService = require('ms-bing-speech-service');
-const options = {
+
+const wiki = require('wikijs').default; //Wikipedia API
+
+const fileUpload = require('express-fileupload'); //File Upload API
+
+const speechService = require('ms-bing-speech-service');//Bing Speech API
+const speechServiceOptions = {
     language: 'en-US',
     subscriptionKey: '94dabe3881d944099d322529affca28e',
     mode: 'dictation'
@@ -29,10 +38,35 @@ app.get("/welcome", function (req, res) {
     res.send(JSON.stringify({a: "Welcome"}));
 });
 
+// app.post('/uploadGoogle', function (req, res) {
+//     res.setHeader('Content-Type', 'application/json');
+//     let filePath = './' + req.files.mFileName.name;
+//     var client = new speech.SpeechClient(configGoogle);
+// // Reads a local audio file and converts it to base64
+//     var file = fs.readFileSync(filePath);
+//     var audioBytes = file.toString('base64');
+//     var request = {
+//         audio: {
+//             content: audioBytes,
+//         },
+//         config: {
+//             encoding: 'LINEAR16',
+//             sampleRateHertz: 8000,
+//             languageCode: 'en-US',
+//         },
+//     };
+// // Detects speech in the audio file
+//     client.recognize(request).then(data => {
+//             var transcription = data[0].results[0].alternatives[0].transcript;
+//             console.log('Transcription:',transcription);
+//             res.send(transcription);
+//         }).catch(err => {
+//             console.error('ERROR:', err);
+//         });
+// });
+
 app.post('/upload', function (req, res) {
     res.setHeader('Content-Type', 'application/json');
-    //res.send(JSON.stringify({a: "send the file ya manyak"}));
-
     let filePath = './' + req.files.mFileName.name;
     let analyzedText;
     let out = {};
@@ -40,11 +74,11 @@ app.post('/upload', function (req, res) {
     //let userLang = req.body.userLanguage;
     let userLang = "fr";
 
-    if(req.files.mFileName.mimetype != "audio/wave" || req.files.mFileName.mimetype != "audio/x-wav"){
+    if (req.files.mFileName.mimetype != "audio/wave") {
         console.log("check: " + req.files.mFileName.mimetype);
         res.send(JSON.stringify({a: req.files.mFileName.mimetype}));
     }
-    SpeechToText(filePath).then(retAnalyzedText => {
+    SpeechToTextGoogle(filePath).then(retAnalyzedText => {
         analyzedText = retAnalyzedText;
         console.log(analyzedText);
         getPhrases(analyzedText).then(retPhrases => {
@@ -52,7 +86,7 @@ app.post('/upload', function (req, res) {
             console.log(phrases);
             wikiLoop(phrases, userLang).then(retWikiTerms => {
                 wikiTerms = retWikiTerms;
-                console.log(phrases);
+                console.log(wikiTerms);
                 out.text = analyzedText;
                 out.terms = phrases;
                 out.wiki = wikiTerms;
@@ -65,9 +99,9 @@ app.post('/upload', function (req, res) {
 app.listen(port);
 
 //Functions
-async function SpeechToText(filePath) {
+async function SpeechToTextMicrosoft(filePath) {
     var retText = await new Promise(resolve => {
-        let recognizer = new speechService(options);
+        let recognizer = new speechService(speechServiceOptions);
         let analyzedText;
         recognizer.start((error, service) => {
             if (!error) {
@@ -78,11 +112,36 @@ async function SpeechToText(filePath) {
                         analyzedText = speechtotext.DisplayText;
                         resolve(analyzedText);
                     } else {
-                        console.log(speechtotext);
-                        console.log(speechtotext.RecognitionStatus);
+                        //console.log(speechtotext);
+                        //console.log(speechtotext.RecognitionStatus);
                     }
                 });
             }
+        });
+    });
+    return retText;
+}
+
+async function SpeechToTextGoogle(filePath) {
+    var retText = await new Promise(resolve => {
+        let client = new speech.SpeechClient(configGoogle);
+        // Reads a local audio file and converts it to base64
+        let file = fs.readFileSync(filePath);
+        let audioBytes = file.toString('base64');
+        let request = {audio: {content: audioBytes,},
+            config: {
+                encoding: 'LINEAR16',
+                sampleRateHertz: 16000,
+                languageCode: 'en-US',
+            },
+        };
+        // Detects speech in the audio file
+        client.recognize(request).then(data => {
+            var transcription = data[0].results[0].alternatives[0].transcript;
+            resolve(transcription);
+        }).catch(err => {
+            console.error('ERROR:', err)
+            resolve(err);
         });
     });
     return retText;
@@ -111,7 +170,7 @@ async function wikiTerm(term, userLang) {
             wiki().page(searched.results[0]).then(page => {
                 try {
                     page.langlinks().then(langsArray => {
-                        if(langsArray.length<5){    //does not return object with less then 5 translations
+                        if (langsArray.length < 5) {    //does not return object with less then 5 translations
                             resolve({});
                         }
                         for (var i = 0; i < langsArray.length; i++) {
