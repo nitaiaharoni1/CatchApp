a5cbabfa5d386bec2c7140bfb886269688973232
nitaiaharoni1 <nitaiaharoni1@gmail.com>
2018-05-02 20:02:30 +0300

    asfsaf

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser');
 
-const fs = require('fs');
+// const fs = require('fs');
+// const fileUpload = require('express-fileupload'); //File Upload API
 
-const speech = require("@google-cloud/speech"); // Google Speech API
-const configGoogle = {
-    projectId: 'dulcet-pilot-202710',
-    keyFilename: './google-speech/Catchapp-24f90e9bc6d3.json'
-};
+
+// const speech = require("@google-cloud/speech"); // Google Speech API
+// const configGoogle = {
+//     projectId: 'dulcet-pilot-202710',
+//     keyFilename: './google-speech/Catchapp-24f90e9bc6d3.json'
+// };
+
+// const speechService = require('ms-bing-speech-service');//Bing Speech API
+// const speechServiceOptions = {
+//     language: 'en-US',
+//     subscriptionKey: '94dabe3881d944099d322529affca28e',
+//     mode: 'dictation'
+//     //format: 'simple'
+// };
 
 const cogserv = require('cogserv-text-analytics'); //Text Analytics
 process.env.COGSERV_TEXT_KEY = '996f7b4c6b924383b3fe595509a7fec1';
@@ -15,22 +25,13 @@ const {keyPhrases, sentiment} = require('./text-analytics')
 
 const wiki = require('wikijs').default; //Wikipedia API
 
-const fileUpload = require('express-fileupload'); //File Upload API
-
-const speechService = require('ms-bing-speech-service');//Bing Speech API
-const speechServiceOptions = {
-    language: 'en-US',
-    subscriptionKey: '94dabe3881d944099d322529affca28e',
-    mode: 'dictation'
-    //format: 'simple'
-};
-
 var app = express();
-app.use(bodyParser.json()); // support json encoded bodies
-app.use(bodyParser.urlencoded({extended: true})); // support encoded bodies
-app.use(fileUpload());
+// app.use(bodyParser.json()); // support json encoded bodies
+// app.use(bodyParser.urlencoded({extended: true})); // support encoded bodies
+// app.use(fileUpload());
 
 var port = process.env.PORT || 8010;
+app.listen(port);
 
 app.get("/terms", function (req, res) {
     res.setHeader('Content-Type', 'application/json');
@@ -38,13 +39,16 @@ app.get("/terms", function (req, res) {
     let userLang = "fr";
     let phrases;
     let text = req.headers.text;
-    console.log(text);
+    if (text.length == 0) {
+        res.send(out);
+    }
+    //console.log(text);
     getPhrases(text).then(retPhrases => {
         phrases = retPhrases;
-        console.log(phrases);
+        //console.log(phrases);
         wikiLoop(phrases, userLang).then(retWikiTerms => {
             wikiTerms = retWikiTerms;
-            console.log(wikiTerms);
+            //console.log(wikiTerms);
             out.text = text;
             out.terms = phrases;
             out.wiki = wikiTerms;
@@ -54,98 +58,53 @@ app.get("/terms", function (req, res) {
 
 });
 
-app.post('/upload', function (req, res) {
-    res.setHeader('Content-Type', 'application/json');
-    let filePath = './' + req.files.mFileName.name;
-    console.log(req.files);
-    let analyzedText;
-    let out = {};
-    let phrases;
-    //let userLang = req.body.userLanguage;
-    let userLang = "fr";
+// app.post('/upload', function (req, res) {
+//     res.setHeader('Content-Type', 'application/json');
+//     let filePath = './' + req.files.mFileName.name;
+//     console.log(req.files);
+//     let analyzedText;
+//     let out = {};
+//     let phrases;
+//     //let userLang = req.body.userLanguage;
+//     let userLang = "fr";
+//     SpeechToTextGoogle(filePath).then(retAnalyzedText => {
+//         analyzedText = retAnalyzedText;
+//         console.log(analyzedText);
+//         getPhrases(analyzedText).then(retPhrases => {
+//             phrases = retPhrases;
+//             console.log(phrases);
+//             wikiLoop(phrases, userLang).then(retWikiTerms => {
+//                 wikiTerms = retWikiTerms;
+//                 console.log(wikiTerms);
+//                 out.text = analyzedText;
+//                 out.terms = phrases;
+//                 out.wiki = wikiTerms;
+//                 res.send(JSON.stringify({a: out}));
+//             });
+//         });
+//     });
+// });
 
-    // if (req.files.mFileName.mimetype != "audio/wave") {
-    //     console.log("check: " + req.files.mFileName.mimetype);
-    //     res.send(JSON.stringify({a: req.files.mFileName.mimetype}));
-    // }
-    SpeechToTextGoogle(filePath).then(retAnalyzedText => {
-        analyzedText = retAnalyzedText;
-        console.log(analyzedText);
-        getPhrases(analyzedText).then(retPhrases => {
-            phrases = retPhrases;
-            console.log(phrases);
-            wikiLoop(phrases, userLang).then(retWikiTerms => {
-                wikiTerms = retWikiTerms;
-                console.log(wikiTerms);
-                out.text = analyzedText;
-                out.terms = phrases;
-                out.wiki = wikiTerms;
-                res.send(JSON.stringify({a: out}));
-            });
-        });
-    });
-});
-
-app.listen(port);
 
 //Functions
-async function SpeechToTextMicrosoft(filePath) {
-    var retText = await new Promise(resolve => {
-        let recognizer = new speechService(speechServiceOptions);
-        let analyzedText;
-        recognizer.start((error, service) => {
-            if (!error) {
-                console.log('Recognition Started!');
-                service.sendFile(filePath);
-                service.on('recognition', (speechtotext) => {
-                    if (speechtotext.RecognitionStatus === 'Success') {
-                        analyzedText = speechtotext.DisplayText;
-                        resolve(analyzedText);
-                    } else {
-                        //console.log(speechtotext);
-                        //console.log(speechtotext.RecognitionStatus);
-                    }
-                });
-            }
-        });
-    });
-    return retText;
-}
-
-async function SpeechToTextGoogle(filePath) {
-    var retText = await new Promise(resolve => {
-        let client = new speech.SpeechClient(configGoogle);
-        // Reads a local audio file and converts it to base64
-        let file = fs.readFileSync(filePath);
-        let audioBytes = file.toString('base64');
-        let request = {
-            audio: {content: audioBytes,},
-            config: {
-                encoding: 'LINEAR16',
-                sampleRateHertz: 16000,
-                languageCode: 'en-US',
-            },
-        };
-        // Detects speech in the audio file
-        client.recognize(request).then(data => {
-            var transcription = data[0].results[0].alternatives[0].transcript;
-            resolve(transcription);
-        }).catch(err => {
-            console.error('ERROR:', err)
-            resolve(err);
-        });
-    });
-    return retText;
-}
-
 async function getPhrases(text) {
     var retPhrases = await new Promise(resolve => {
         let phrases;
         keyPhrases(text).then(resPhrases => {
             resPhrases = JSON.parse(resPhrases);
-            phrases = resPhrases.documents[0].keyPhrases;
+            try {
+                phrases = resPhrases.documents[0].keyPhrases;
+                resolve(phrases);
+            } catch (error) {
+                console.error(err);
+                phrases = [];
+                resolve(phrases);
+            }
+        }).catch(err => {
+            console.error(err);
+            phrases = [];
             resolve(phrases);
-        }).catch(err => console.error(err));
+        });
     });
     return retPhrases;
 }
@@ -157,21 +116,24 @@ async function wikiTerm(term, userLang) {
         let obj = {};
         let counter = 0;
         wiki().search(term, 1).then(searched => {
+            if (searched.results.length == 0) {
+                resolve(obj);
+            }
             //which result to choose??? 0, 1 or 2?
             wiki().page(searched.results[0]).then(page => {
                 try {
                     page.langlinks().then(langsArray => {
-                        if (langsArray.length < 5) {    //does not return object with less then 5 translations
-                            resolve({});
+                        if (langsArray.length < 3) {    //does not return object with less then 5 translations
+                            resolve(obj);
                         }
+                        langSearch = searched.results[0];
+                        langCountry = "en";
+                        var j = langsArray.indexOf("Apple");
                         for (var i = 0; i < langsArray.length; i++) {
                             if (langsArray[i].lang == userLang) {
                                 langSearch = langsArray[i].title;
                                 langCountry = userLang;
                                 break;
-                            } else {
-                                langSearch = searched.results[0];
-                                langCountry = "en";
                             }
                         }
                         wiki({apiUrl: 'http://' + langCountry + '.wikipedia.org/w/api.php'}).page(langSearch).then(page => {
@@ -179,27 +141,23 @@ async function wikiTerm(term, userLang) {
                             obj.url = page.raw.fullurl;
                             page.summary().then(summary => {
                                 obj.summery = summary;
-                                counter = counter + 0.5;
-                                if (counter == 1) {
+                                if (obj.image != undefined) {
                                     resolve(obj);
                                 }
                             }).catch(err => {
                                 obj.summery = "";
-                                counter = counter + 0.5;
-                                if (counter == 1) {
+                                if (obj.image != undefined) {
                                     resolve(obj);
                                 }
                             });
                             page.mainImage().then(mainImage => {
                                 obj.image = mainImage;
-                                counter = counter + 0.5;
-                                if (counter == 1) {
+                                if (obj.summery != undefined) {
                                     resolve(obj);
                                 }
                             }).catch(err => {
                                 obj.image = "";
-                                counter = counter + 0.5;
-                                if (counter == 1) {
+                                if (obj.summery != undefined) {
                                     resolve(obj);
                                 }
                             });
@@ -209,6 +167,9 @@ async function wikiTerm(term, userLang) {
                     console.error(error);
                 }
             });
+        }).catch(err => {
+            console.error(err);
+            resolve(obj);
         });
     });
     return retWikiTerm;
@@ -230,3 +191,50 @@ async function wikiLoop(phrases, userLang) {
     });
     return retWikiTerms;
 }
+
+// async function SpeechToTextMicrosoft(filePath) {
+//     var retText = await new Promise(resolve => {
+//         let recognizer = new speechService(speechServiceOptions);
+//         let analyzedText;
+//         recognizer.start((error, service) => {
+//             if (!error) {
+//                 console.log('Recognition Started!');
+//                 service.sendFile(filePath);
+//                 service.on('recognition', (speechtotext) => {
+//                     if (speechtotext.RecognitionStatus === 'Success') {
+//                         analyzedText = speechtotext.DisplayText;
+//                         resolve(analyzedText);
+//                     } else {
+//                     }
+//                 });
+//             }
+//         });
+//     });
+//     return retText;
+// }
+//
+// async function SpeechToTextGoogle(filePath) {
+//     var retText = await new Promise(resolve => {
+//         let client = new speech.SpeechClient(configGoogle);
+//         // Reads a local audio file and converts it to base64
+//         let file = fs.readFileSync(filePath);
+//         let audioBytes = file.toString('base64');
+//         let request = {
+//             audio: {content: audioBytes,},
+//             config: {
+//                 encoding: 'LINEAR16',
+//                 sampleRateHertz: 16000,
+//                 languageCode: 'en-US',
+//             },
+//         };
+//         // Detects speech in the audio file
+//         client.recognize(request).then(data => {
+//             var transcription = data[0].results[0].alternatives[0].transcript;
+//             resolve(transcription);
+//         }).catch(err => {
+//             console.error('ERROR:', err)
+//             resolve(err);
+//         });
+//     });
+//     return retText;
+// }
